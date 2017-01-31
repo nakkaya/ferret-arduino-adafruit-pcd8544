a636d3c2582cc54d2777e1ea84dd60ebd8f0f7c1
Nurullah Akkaya <nurullah@nakkaya.com>
2017-01-31 23:49:40 +0300

    update for ferret 0.2.6

--- core.clj
+++ core.clj
@@ -7,79 +7,79 @@
                      " = Adafruit_PCD8544(" sclk ", " din ", " dc ", " cs ", " rst ");")]
     `(~'do
        (~'native-declare ~lcd-dec)
-       (~'cxx ~(str "__result = obj<Pointer>(&" lcd-sym ");")))))
+       (~'cxx ~(str "__result = obj<pointer>(&" lcd-sym ");")))))
 
 (defn begin [scr]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
    s->begin();")
 
 (defn contrast [scr cont]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
-   number_t contrast = cont.to<number_t>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
+   number_t contrast = number::to<number_t>(cont);
    s->setContrast(contrast);")
 
 (defn text-size [scr size]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
-   number_t _size = size.to<number_t>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
+   number_t _size = number::to<number_t>(size);
    s->setContrast(_size);")
 
 (defn text-color-black [scr]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
    s->setTextColor(BLACK);")
 
 (defn clear [scr]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
    s->clearDisplay();")
 
 (defn display [scr]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
    s->display();")
 
 (defn cursor [scr x y]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
-   number_t _x = x.to<number_t>();
-   number_t _y = y.to<number_t>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
+   number_t _x = number::to<number_t>(x);
+   number_t _y = number::to<number_t>(y);
    s->setCursor(_x, _y);")
 
 (defn println [scr msg]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
 
-   if (msg.is_type(runtime::type::Number))
-     s->println(msg.to<number_t>());
-   else if (msg.is_type(runtime::type::String)){
+   if (msg.is_type(runtime::type::number))
+     s->println(number::to<number_t>(msg));
+   else if (msg.is_type(runtime::type::string)){
      char buff [80] = {0};
      int idx = 0;
      for(auto it : runtime::range(msg)){
-      buff[idx++] = it.to<char>();
+      buff[idx++] = number::to<char>(it);
      }
      s->println(buff);
    }")
 
 (defn print [scr msg]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
 
-   if (msg.is_type(runtime::type::Number)){
-     if (runtime::abs(msg.to<number_t>() - msg.to<real_t>()) < FERRET_REAL_EPSILON)
-       s->print(msg.to<number_t>());
+   if (msg.is_type(runtime::type::number)){
+     if (runtime::abs(number::to<number_t>(msg) - number::to<real_t>(msg)) < FERRET_REAL_EPSILON)
+       s->print(number::to<number_t>(msg));
      else
-       s->print(msg.to<real_t>());
-   }else if (msg.is_type(runtime::type::String)){
+       s->print(number::to<real_t>(msg));
+   }else if (msg.is_type(runtime::type::string)){
      char buff [80] = {0};
      int idx = 0;
      for(auto it : runtime::range(msg)){
-      buff[idx++] = it.to<char>();
+      buff[idx++] = number::to<char>(it);
      }
      s->print(buff);
    }")
 
 (defn width [scr]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
-   __result = obj<Number>(s->width());")
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
+   __result = obj<number>(s->width());")
 
 (defn draw-line-black [scr x1 y1 x2 y2]
-  "Adafruit_PCD8544 *s = scr.cast<Pointer>()->pointer<Adafruit_PCD8544>();
-   number_t _x1 = x1.to<number_t>();
-   number_t _x2 = x2.to<number_t>();
-   number_t _y1 = y1.to<number_t>();
-   number_t _y2 = y2.to<number_t>();
+  "Adafruit_PCD8544 *s = pointer::to_pointer<Adafruit_PCD8544>(scr);
+   number_t _x1 = number::to<real_t>(x1);
+   number_t _x2 = number::to<real_t>(x2);
+   number_t _y1 = number::to<real_t>(y1);
+   number_t _y2 = number::to<real_t>(y2);
    s->drawLine(_x1, _y1,_x2,_y2,BLACK);")
